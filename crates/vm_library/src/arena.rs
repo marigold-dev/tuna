b7a49ba1d76101328ef94d272a0e8795aaff2e3f
Jules Guesnon <36401048+JulesGuesnon@users.noreply.github.com>
2022-09-22 15:15:16 +0200

    refactor: Added clippy and changed release build (#23)

--- crates/vm_library/src/arena.rs
+++ crates/vm_library/src/arena.rs
@@ -14,16 +14,16 @@ pub static mut CONSTANTS: Lazy<Vec<Value>> = Lazy::new(|| Vec::with_capacity(300
 pub fn populate_predef(sender: String, self_: String, source: String) {
     let map = unsafe { &mut PREDEF };
     map.clear();
-    map.insert("none".to_string(), Value::Option(None));
-    map.insert("unit".to_string(), Value::Unit);
+    map.insert("none".to_owned(), Value::Option(None));
+    map.insert("unit".to_owned(), Value::Unit);
 
-    map.insert("nil".to_string(), Value::List(vector![]));
-    map.insert("source".to_string(), Value::String(source));
-    map.insert("sender".to_string(), Value::String(sender));
-    map.insert("self".to_string(), Value::String(self_));
-    map.insert("empty_set".to_string(), Value::Set(ordset![]));
-    map.insert("empty_map".to_string(), Value::Map(ordmap! {}));
-    map.insert("zero".to_string(), Value::Int(0.into()));
+    map.insert("nil".to_owned(), Value::List(vector![]));
+    map.insert("source".to_owned(), Value::String(source));
+    map.insert("sender".to_owned(), Value::String(sender));
+    map.insert("self".to_owned(), Value::String(self_));
+    map.insert("empty_set".to_owned(), Value::Set(ordset![]));
+    map.insert("empty_map".to_owned(), Value::Map(ordmap! {}));
+    map.insert("zero".to_owned(), Value::Int(0.into()));
 }
 pub fn push_constants(vec: Vec<(i32, Value)>) {
     let map = unsafe { &mut CONSTANTS };
--- crates/vm_library/src/lib.rs
+++ crates/vm_library/src/lib.rs
@@ -1,3 +1,24 @@
+#![warn(
+    clippy::perf,
+    clippy::complexity,
+    clippy::correctness,
+    clippy::todo,
+    clippy::needless_continue,
+    clippy::needless_borrow,
+    clippy::if_let_mutex,
+    clippy::suboptimal_flops,
+    clippy::lossy_float_literal,
+    clippy::fn_params_excessive_bools,
+    clippy::inefficient_to_string,
+    clippy::macro_use_imports,
+    clippy::option_option,
+    clippy::unnested_or_patterns,
+    clippy::str_to_string,
+    clippy::cast_lossless,
+    clippy::implicit_clone,
+    clippy::redundant_closure_for_method_calls
+)]
+
 pub mod arena;
 pub mod compile_store;
 pub(crate) mod conversions;
